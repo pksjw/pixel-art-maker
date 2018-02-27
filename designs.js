3633f105795c44279ce5ab4c964d308ad8e37152
pksjw <pks_jw@msn.com>
2018-02-27 00:48:32 -0500

    Add background and other UI tweaking

--- designs.js
+++ designs.js
@@ -1,7 +1,6 @@
 // Select color input
 // Select size input
 
-
 // When size is submitted by the user, call makeGrid()
 $("input[type='submit']").on('click', function makeGrid(event) {
     // prevent page refreshing when clicking submit
@@ -12,6 +11,7 @@ $("input[type='submit']").on('click', function makeGrid(event) {
     let columns = $("#inputWidth").val();
     grid.children().remove(); // clean up any previous table rows that exsit
 
+//Build the grid row by row
     for (let r=1; r <= rows; r++) {
         let tableRow = '<tr>';
         for (let c=1; c <= columns; c++) {
@@ -20,6 +20,7 @@ $("input[type='submit']").on('click', function makeGrid(event) {
         tableRow += '</tr>';
         $(grid).append(tableRow); // add grid to DOM
     }
+// Listen for click and double click on the grid
 
     grid.on("dblclick", "td", function(event) {
         $(this).css("background-color", "transparent");
