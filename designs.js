a539992cc1dc5774fc3af07e28fbd66e15844ab8
pksjw <pks_jw@msn.com>
2018-03-14 17:06:16 -0400

    Refactored js, UI enhancements, added continuous draw and erase

--- designs.js
+++ designs.js
@@ -3,31 +3,85 @@
 
 // When size is submitted by the user, call makeGrid()
 
-$("input[type='submit']").on('click', function makeGrid(event) {
+// Set the inital 'paint' changes happen in click event
+
+let gridTileMode = "PAINT" // controls paint or erase of grid cells (td's)
+
+$('input[type="submit"]').on('click', function makeGrid(event) {
     // prevent page refreshing when clicking submit
     event.preventDefault();
-
+    let mouseIsDown = false;
     let grid = $('#pixelCanvas');
     let rows = $("#inputHeight").val();
     let columns = $("#inputWidth").val();
+
     grid.children().remove(); // delete any previous table rows
 
-//Build the grid row by row
+//Build the grid row by row and then append to the table
+//  project rubrics requires use of for and while loops
 
-    for (let r=1; r <= rows; r++) {
-        let tableRow = '<tr>';
+    let tableRows = '';
+    let r = 1;
+    while (r <= rows) {
+        tableRows += '<tr>';
         for (let c=1; c <= columns; c++) {
-            tableRow += '<td></td>';
+            tableRows += '<td></td>';
         }
-        tableRow += '</tr>';
-        $(grid).append(tableRow); // add grid to DOM
-    }
-// Listen for click and double click on the grid
+        tableRows += '</tr>';
+        r += 1;
+    } // end while loop
+    $(grid).append(tableRows); // add grid to DOM
+    $('.legend').show(); // <p> tag with instructions for mouseover
 
-    grid.on("dblclick", "td", function(event) {
-        $(this).css("background-color", "transparent");
+// Listen for click to paint or erase a tile
+
+    grid.on('click', 'td', function() {
+        paintEraseTiles($(this));
     });
-    grid.on("click", "td", function() {
-        $(this).css("background-color", $("#colorPicker").val());
+
+// Listen for mouse down, up and over for continuous paint and erase
+
+    grid.on('mousedown', function(event) {
+        event.preventDefault();
+        event.which === 1 ? mouseIsDown = true : mouseIsDown = false;
     });
-});
+
+    grid.on('mouseup', function() {
+        mouseIsDown = false;
+    });
+
+    grid.on('mouseover', 'td', function() {
+        if (mouseIsDown) {paintEraseTiles($(this));}
+    }); // end continuous paint and erase
+}); // end grid
+
+// paint or erase cells based on the mode (girdTileMode)
+
+function paintEraseTiles(targetCell) {
+        if (gridTileMode === 'PAINT') {
+        $(targetCell).css('background-color', $('#colorPicker').val());
+    } else if (gridTileMode === 'ERASE') {
+        $(targetCell).css('background-color', 'transparent');
+    }
+}
+
+    $('#inputHeight').on('input', function() {
+        $('#gridHeightDisplay').text(' ' + $(this).val());
+    });
+
+    $('#inputWidth').on('input', function() {
+        $('#gridWidthDisplay').text(' ' + $(this).val());
+    });
+
+    $('#colorPicker').on('input', function() {
+        gridTileMode = 'PAINT';
+        $('.paintOrErase').text(' ' + gridTileMode);
+    });
+
+// set the mode to PAINT or ERASE
+
+    $('button').on('click', function(event) {
+        let idClicked = event.currentTarget.id;
+        idClicked === 'paintBtn' ? gridTileMode = 'PAINT' : gridTileMode = 'ERASE';
+        $('.paintOrErase').text(' ' + gridTileMode);
+    }); // end button on click
